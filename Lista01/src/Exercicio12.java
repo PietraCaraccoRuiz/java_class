be53d8ba95ff353db27383457d27ddd653828177
PietraCaraccoRuiz <caraccoruizpietra@gmail.com>
2025-05-23 11:24:53 -0300

    Atualizando Exercicio13

--- Lista01/src/Exercicio12.java
+++ Lista01/src/Exercicio12.java
@@ -3,21 +3,39 @@ import java.util.Scanner;
 public class Exercicio12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
+        int valido = 0;
 
         System.out.println("Digite seu e-mail: ");
         String email = scanner.nextLine();
 
-        int posicaoArroba = email.indexOf("@");
-        int posicaoPonto = email.indexOf(".");
-        String dominio1 = email.substring(posicaoArroba+1);
-        String dominio2 = email.substring(posicaoArroba+1, posicaoPonto+1);
-        System.out.println("1: "+ dominio1);
-        System.out.println("2: "+dominio2);
-//
-//        if (email.contains("@") && dominio2.contains(".") && posicao != 0) {
-//            System.out.println("Endereço de e-mail válido.");
-//        } else {
-//            System.out.println("Endereço de e-mail inválido!");
-//        }
+        if (email.contains("@")){
+            if (email.contains(".")) {
+                int posicaoArroba = email.indexOf("@");
+                int posicaoPonto = email.indexOf(".");
+                String dominio1 = email.substring(posicaoArroba + 1, posicaoPonto);
+                String dominio2 = email.substring(posicaoPonto + 1);
+
+                if (dominio1.isEmpty()) {
+                    System.out.println("E-mail não possui dominio depois do '@'");
+                } else {
+                    valido += 1;
+                }
+
+                if (dominio2.isEmpty()) {
+                    System.out.println("E-mail não possui dominio depois do '.'");
+                } else {
+                    valido += 1;
+                }
+
+            } else {
+                System.out.println("\nNão contem um domínio precisa de '.'");
+            }
+        } else {
+            System.out.println("\nNão contem um domínio precisa de '@'");
+        }
+
+        if (valido == 2) {
+            System.out.println("E-mail válido!");
+        }
     }
 }
--- Lista01/src/Exercicio13.java
+++ Lista01/src/Exercicio13.java
@@ -5,8 +5,14 @@ public class Exercicio13 {
         Scanner scanner = new Scanner (System.in);
 
         System.out.println("Digite o seu tipo de moeda: ");
+        System.out.println("1 - Real ");
+        System.out.println("2 - Dólar ");
+        System.out.println("1 - Euro ");
         int moeda_inicial = Integer.parseInt(scanner.nextLine());
         System.out.println("Digite o seu tipo de moeda para conversão: ");
+        System.out.println("1 - Real ");
+        System.out.println("2 - Dólar ");
+        System.out.println("1 - Euro ");
         int moeda_final = Integer.parseInt(scanner.nextLine());
     }
 }
